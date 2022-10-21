6fb535cbcde51f04b784b83d4545a3e16edda7d4
ochi <ocos54ce@gmail.com>
2022-10-21 23:27:41 +0900

    chapter7-2

---
+++ chapter7-2.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from sklearn import tree
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('./datafiles/Survived.csv')
+# print(df['Survived'].value_counts())
+# print(df.isnull().sum())
+# print(df.shape)
+
+df['Age'] = df['Age'].fillna(df['Age'].mean())
+df['Embarked'] = df['Embarked'].fillna(df['Embarked'].mode()[0])
+
+col = ['Pclass', 'Age', 'SibSp', 'Parch', 'Fare']
+x = df[col]
+t = df['Survived']
+
+x_train, x_test, y_train, y_test = train_test_split(x,t,test_size=0.2, random_state=0)
+print(x_train.shape)
